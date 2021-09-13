a13ff533cbad115ad9b3af22ad8f07088895e706
danielvazsonyi <daniel.vazsonyi@balabit.com>
2021-09-13 14:13:30 +0200

    add dummy app

---
+++ apps/demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'meetup-cypress-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'demo';
+}
