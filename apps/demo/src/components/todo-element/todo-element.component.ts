6351cf445fda41b9fd2e6bc94464df1a5dbaa523
vazsonyidl <danielvazsonyi@gmail.com>
2021-12-22 09:39:59 +0100

    tmp

--- apps/demo/src/components/todo-element/todo-element.component.ts
+++ apps/demo/src/components/todo-element/todo-element.component.ts
@@ -3,8 +3,15 @@ import { Component, Input } from "@angular/core";
 @Component({
   selector: 'app-todo-element',
   template: `
-    <div data-testid="todo-element">{{element}}</div>
-  `
+    <mat-card data-testid="todo-element">{{element}}</mat-card>
+  `,
+  styles: [
+    `
+      mat-card {
+        margin: 8px 0;
+      }
+    `
+  ]
 })
 export class TodoElementComponent {
   @Input() element: string;
--- apps/demo/src/components/todo-element/todo-element.module.ts
+++ apps/demo/src/components/todo-element/todo-element.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from "@angular/core";
 import { TodoElementComponent } from "./todo-element.component";
+import { MatCardModule } from "@angular/material/card";
 
 @NgModule({
   declarations: [TodoElementComponent],
+  imports: [
+    MatCardModule
+  ],
   exports: [TodoElementComponent]
 })
 export class TodoElementModule {
